a2f189ea674d6dc052fcbc287a230f9f1915365b
Ayaka Neko <neko@ayaka.moe>
2020-08-27 15:33:12 +0800

    :recycle: Organized code with ESLint
    Signed-off-by: Ayaka Neko <neko@ayaka.moe>

--- src/util/hash.js
+++ src/util/hash.js
@@ -1,12 +1,12 @@
-const crypto = require("crypto")
+const crypto = require('crypto')
 
-function sha256(string) {
-    let str = string
-    for (let i = 0; i < 4; i++) {
-        str += Math.floor(Math.random() * Math.floor(9))
-    }
-    let hash = crypto.createHash('sha256').update(str, "utf8").digest("hex")
-    return hash
+function sha256 (string) {
+  let str = string
+  for (let i = 0; i < 4; i++) {
+    str += Math.floor(Math.random() * Math.floor(9))
+  }
+  let hash = crypto.createHash('sha256').update(str, 'utf8').digest('hex')
+  return hash
 }
 
 exports.sha256 = sha256
