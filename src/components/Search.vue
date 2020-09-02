8a2a59ea71fd3a35eaf964eb1b5eb81cbd5c12f3
Ayaka Neko <neko@ayaka.moe>
2020-09-02 14:53:16 +0800

    :sparkles: Added search bar and sign-in function
    Signed-off-by: Ayaka Neko <neko@ayaka.moe>

---
+++ src/components/Search.vue
@@ -0,0 +1,137 @@
+<template>
+  <div data-app="true">
+    <v-autocomplete
+      v-model="select"
+      :loading="loading"
+      :items="items"
+      :search-input.sync="search"
+      cache-items
+      class="mx-4"
+      flat
+      hide-no-data
+      hide-details
+      label="Search address / user / music"
+      solo-inverted
+      background-color="#333333"
+    >
+    </v-autocomplete>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      loading: false,
+      items: [],
+      search: null,
+      select: null,
+      states: [
+        'Alabama',
+        'Alaska',
+        'American Samoa',
+        'Arizona',
+        'Arkansas',
+        'California',
+        'Colorado',
+        'Connecticut',
+        'Delaware',
+        'District of Columbia',
+        'Federated States of Micronesia',
+        'Florida',
+        'Georgia',
+        'Guam',
+        'Hawaii',
+        'Idaho',
+        'Illinois',
+        'Indiana',
+        'Iowa',
+        'Kansas',
+        'Kentucky',
+        'Louisiana',
+        'Maine',
+        'Marshall Islands',
+        'Maryland',
+        'Massachusetts',
+        'Michigan',
+        'Minnesota',
+        'Mississippi',
+        'Missouri',
+        'Montana',
+        'Nebraska',
+        'Nevada',
+        'New Hampshire',
+        'New Jersey',
+        'New Mexico',
+        'New York',
+        'North Carolina',
+        'North Dakota',
+        'Northern Mariana Islands',
+        'Ohio',
+        'Oklahoma',
+        'Oregon',
+        'Palau',
+        'Pennsylvania',
+        'Puerto Rico',
+        'Rhode Island',
+        'South Carolina',
+        'South Dakota',
+        'Tennessee',
+        'Texas',
+        'Utah',
+        'Vermont',
+        'Virgin Island',
+        'Virginia',
+        'Washington',
+        'West Virginia',
+        'Wisconsin',
+        'Wyoming'
+      ]
+    }
+  },
+  watch: {
+    search (val) {
+      val && val !== this.select && this.querySelections(val)
+    }
+  },
+  methods: {
+    querySelections (v) {
+      this.loading = true
+      // Simulated ajax query
+      setTimeout(() => {
+        this.items = this.states.filter(e => {
+          return (e || '').toLowerCase().indexOf((v || '').toLowerCase()) > -1
+        })
+        this.loading = false
+      }, 500)
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+/deep/ .v-label.theme--light{
+  color: white;
+}
+
+/deep/ .v-icon.mdi-menu-down.theme--light {
+  color: white;
+}
+
+/deep/ .v-list-item__title {
+  text-align: left;
+}
+
+/deep/ .v-select-list.theme--light {
+  background-color: rgba(51,51,51,0.5);
+}
+
+/deep/ .v-list-item__title {
+  color: white;
+}
+
+/deep/ .v-list-item__mask {
+color: white !important;
+  background-color: #E56D9B !important;
+}
+</style>
