92cee2667091cfb21a7aa418306c32935cfd5f13
Ayaka Neko <neko@ayaka.moe>
2020-09-10 16:46:55 +0800

    :sparkles: Added about page for more description for our project
    Signed-off-by: Ayaka Neko <neko@ayaka.moe>

---
+++ src/pages/About.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <spaceLayout>
+    </spaceLayout>
+  </div>
+</template>
+
+<script>
+import spaceLayout from '@/components/Layout/Space'
+
+export default {
+  components: {
+    spaceLayout
+  },
+  mounted () {
+    document.title = 'Why Choose ArcLight - ArcLight Music Distribution Platform'
+  }
+}
+</script>
