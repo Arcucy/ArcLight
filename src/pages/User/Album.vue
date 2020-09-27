e94ac3df941238a9bbed54caca16148a130ec51a
LittleSound <464388324@qq.com>
2020-09-27 16:40:08 +0800

    ✨ My album page

---
+++ src/pages/User/Album.vue
@@ -0,0 +1,16 @@
+<template>
+  <userAllSellings
+    type="album"
+    label="Album"
+    :pagesize="10"
+  />
+</template>
+
+<script>
+import userAllSellings from '@/components/User/UserAllSellings.vue'
+export default {
+  components: {
+    userAllSellings
+  }
+}
+</script>
