593118e9f3b98f73397291c4e4ce92abbc81381f
Ayaka Neko <neko@ayaka.moe>
2020-11-20 15:01:33 +0800

    :wrench: Updated vue config file due to ESLint change
    Signed-off-by: Ayaka Neko <neko@ayaka.moe>

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,16 @@
 module.exports = {
   configureWebpack: {
     performance: {
-      hints:'warning',
-      //入口起点的最大体积
+      hints: 'warning',
+      // 入口起点的最大体积
       maxEntrypointSize: 50000000,
-      //生成文件的最大体积
+      // 生成文件的最大体积
       maxAssetSize: 30000000,
-      //只给出 js 文件的性能提示
-      assetFilter: function(assetFilename) {
-        return assetFilename.endsWith('.js');
+      // 只给出 js 文件的性能提示
+      assetFilter: function (assetFilename) {
+        return assetFilename.endsWith('.js')
       }
-    },
+    }
   },
   publicPath: process.env.NODE_ENV === 'production'
     ? '././'
